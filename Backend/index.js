c6d666da88ba49526872ce36a23a020bea45c44c
rkhexed <64305387+rkhexed@users.noreply.github.com>
2024-11-01 19:19:28 +0530

    Added import statements and a skeleton call for ML API

--- Backend/index.js
+++ Backend/index.js
@@ -8,3 +8,25 @@ const { Server } = require('socket.io');
 const harperSaveMessage = require('./services/harper-save-message');
 const harperGetMessages = require('./services/harper-get-messages');
 const leaveRoom = require('./utils/leave-room');
+
+socket.on('send_message', async (data) => {
+    const { message, username, room, __createdtime__ } = data;
+
+    try {
+        // Call ML translation API
+        const response = await axios.post('http://localhost:8000/translate/', {
+            text: message,
+            source_lang: 'en', // add feature for users to set their language
+            target_lang: 'es'  // target language of receiver, chat room implementation will have to be looked into oops
+        });
+        const translatedMessage = response.data.translated_text;
+
+        // Emit the translated message to all users in the room
+        io.in(room).emit('receive_message', { ...data, message: translatedMessage });
+        
+        // Save the original message and translated message in the database, need more understanding of HarperDB
+        await harperSaveMessage(message, username, room, __createdtime__);
+    } catch (error) {
+        console.error("Error translating message:", error);
+    }
+});
